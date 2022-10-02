bbfaed90d042c864d2104ae18d346cf2060004a7
blanana <79705799+blanana@users.noreply.github.com>
2022-10-02 17:54:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -162,28 +162,23 @@ def ActionChance(x,y):
         chance = 4
         return chance
 
-def control(com1, com2, a, b, x1, x2, y1, y2):
+def control(com1, com2, x1, y1, x2, y2):
     if com1 == 1:
         if com2 == 0:
-            a = b
             pydirectinput.moveTo(x1, y1)
             time.sleep(0.44)
             mouse.click('left')
             pydirectinput.moveTo(x2, y2)
 
-def please_stop():
-    global stop
-    if stop == 0:
-        stop = 1
-        print('commmands diabled')
-        time.sleep(0.2)
-        return
-
-    if stop == 1:
-        stop = 0
-        print('commmands enabled')
-        time.sleep(0.2)
-        return
+def toggle(var, text, val, a, b):
+    if var == a:
+        print(text + ' disabled')
+        time.sleep(val)
+        return b
+    else:
+        print(text + ' enabled')
+        time.sleep(val)
+        return a
 
 # -----------------------------------------------------------------------------------------------------------------------
 # hotkeys
@@ -220,84 +215,35 @@ def hotkey():
 
     # Left door toggle
         if keyboard.is_pressed('1'):
-            if Ldoor == 1:
-                Ldoor = 0
-                print('Left door disabled')
-                time.sleep(0.2)
-            else:
-                Ldoor = 1
-                print('Left door enabled')
-                time.sleep(0.2)
+            Ldoor = toggle(Ldoor, 'Left door', 0.2, 1, 0)
 
     # Right door toggle
         if keyboard.is_pressed('2'):
-            if Rdoor == 1:
-                Rdoor = 0
-                print('Right door disabled')
-                time.sleep(0.2)
-            else:
-                Rdoor = 1
-                print('Right door enabled')
-                time.sleep(0.2)
+            Rdoor = toggle(Rdoor, 'Right door', 0.2, 1, 0)
 
     # Left light toggle
         if keyboard.is_pressed('3'):
-            if Llight == 1:
-                Llight = 0
-                print('Left light disabled')
-                time.sleep(0.2)
-            else:
-                Llight = 1
-                print('Left light enabled')
-                time.sleep(0.2)
+            Llight = toggle(Llight, 'Left light', 0.2, 1, 0)
 
     # Right light toggle
         if keyboard.is_pressed('4'):
-            if Rlight == 1:
-                Rlight = 0
-                print('Right light disabled')
-                time.sleep(0.2)
-            else:
-                Rlight = 1
-                print('Right light enabled')
-                time.sleep(0.2)
+            Rlight = toggle(Rlight, 'Right light', 0.2, 1, 0)
 
     # Camera toggle
         if keyboard.is_pressed('5'):
-            if cameras == 1:
-                cameras = 0
-                print('Cameras diabled')
-                time.sleep(0.2)
-            else:
-                cameras = 1
-                print('Cameras enabled')
-                time.sleep(0.2)
+            cameras = toggle(cameras, 'Cameras', 0.2, 1, 0)
 
     # Boop toggle
         if keyboard.is_pressed('6'):
-            if boop == 1:
-                boop = 0
-                print('Boop disabled (How could you?)')
-                time.sleep(0.2)
-            else:
-                boop = 1
-                print('Boop enabled (boop)')
-                time.sleep(0.2)
+            boop = toggle(boop, 'Boop', 0.2, 1, 0)
 
     # Chance toggle
         if keyboard.is_pressed('7'):
-            if ran == 1:
-                ran = 0
-                print('Chance disabled')
-                time.sleep(0.2)
-            else:
-                ran = 1
-                print('Chance enabled')
-                time.sleep(0.2)
+            ran = toggle(ran, 'Chance', 0.2, 1, 0)
 
     # command toggle
         if keyboard.is_pressed('l'):
-            please_stop()
+            stop = toggle(stop, 'Commands', 0.2, 0, 1)
 
     # reset
         if keyboard.is_pressed('r'):
@@ -385,126 +331,70 @@ def process_input(message):
 # ---------------------------------------------------------------------------
 
     if user.lower() == 'blanana_m' or 'astralspiff':
-        match message:
+        match message.lower():
 
         # toggle individual cameras
-                case "s_start":
-                    if cam1a == 1:
-                        cam1a = 0
-                        print('start disabled')
-                    else:
-                        cam1a = 1
-                        print('start enabled')
+                case "t_cam1a":
+                    cam1a = toggle(cam1a, 'Camera 1a', 0, 1, 0)
 
-                case "s_cam1b":
-                    if cam1b == 1:
-                        cam1b = 0
-                        print('cam1b disabled')
-                    else:
-                        cam1b = 1
-                        print('cam1b enabled')
+                case "t_cam1b":
+                    cam1b = toggle(cam1b, 'Camera 1b', 0, 1, 0)
 
-                case "s_foxy":
-                    if cam1c == 1:
-                        cam1c = 0
-                        print('foxy disabled')
-                    else:
-                        cam1c = 1
-                        print('foxy enabled')
+                case "t_cam1c":
+                    cam1c = toggle(cam1c, 'Camera 1c', 0, 1, 0)
 
-                case "s_cam2a":
-                    if cam2a == 1:
-                        cam2a = 0
-                        print('cam2a disabled')
-                    else:
-                        cam2a = 1
-                        print('cam2a enabled')
+                case "t_cam2a":
+                    cam2a = toggle(cam2a, 'Camera 2a', 0, 1, 0)
 
-                case "s_cam2b":
-                    if cam2b == 1:
-                        cam2b = 0
-                        print('cam2b disabled')
-                    else:
-                        cam2b = 1
-                        print('cam2b enabled')
+                case "t_cam2b":
+                    cam2b = toggle(cam2b, 'Camera 2b', 0, 1, 0)
 
-                case "s_cam3":
-                    if cam3 == 1:
-                        cam3 = 0
-                        print('cam3 disabled')
-                    else:
-                        cam3 = 1
-                        print('cam3 enabled')
+                case "t_cam3":
+                    cam3  = toggle(cam3, 'Camera 3', 0, 1, 0)
 
-                case "s_cam4a":
-                    if cam4a == 1:
-                        cam4a = 0
-                        print('cam4a disabled')
-                    else:
-                        cam4a = 1
-                        print('cam4a enabled')
+                case "t_cam4a":
+                    cam4a = toggle(cam4a, 'Camera 4a', 0, 1, 0)
 
-                case "s_cam4b":
-                    if cam4b == 1:
-                        cam4b = 0
-                        print('cam4b disabled')
-                    else:
-                        cam4b = 1
-                        print('cam4b enabled')
+                case "t_cam4b":
+                    cam4b = toggle(cam4b, 'Camera 4b', 0, 1, 0)
 
-                case "s_cam5":
-                    if cam5 == 1:
-                        cam5 = 0
-                        print('cam5 disabled')
-                    else:
-                        cam5 = 1
-                        print('cam5 enabled')
+                case "t_cam5":
+                    cam5  = toggle(cam5, 'Camera 5', 0, 1, 0)
 
-                case "s_cam6":
-                    if cam6 == 1:
-                        cam6 = 0
-                        print('cam6 disabled')
-                    else:
-                        cam6 = 1
-                        print('cam6 enabled')
+                case "t_cam6":
+                    cam6  = toggle(cam6, 'Camera 6', 0, 1, 0)
 
-                case "s_cam7":
-                    if cam7 == 1:
-                        cam7 = 0
-                        print('cam7 disabled')
-                    else:
-                        cam7 = 1
-                        print('cam7 enabled')
+                case "t_cam7":
+                    cam7  = toggle(cam7, 'Camera 7', 0)
 
 
-                case "s_cam_all":
-                    if cam1a == 1 or cam1b == 1 or cam1c == 1 or cam2a == 1 or cam2b ==1 or cam3 == 1 or cam4a == 1 or cam5 == 1 or cam6 == 1 or cam7 == 1:
-                        cam1a = 0
-                        cam1b = 0
-                        cam1c = 0
-                        cam2a = 0
-                        cam2b = 0
-                        cam3  = 0
-                        cam4a = 0
-                        cam4b = 0
-                        cam5  = 0
-                        cam6  = 0
-                        cam7  = 0
-                        print('all cams disabled')
+                case "e_cam_all":
+                    cam1a = 1
+                    cam1b = 1
+                    cam1c = 1
+                    cam2a = 1
+                    cam2b = 1
+                    cam3  = 1
+                    cam4a = 1
+                    cam4b = 1
+                    cam5  = 1
+                    cam6  = 1
+                    cam7  = 1
+                    print('all cams enabled')
 
-                    else:
-                        cam1a = 1
-                        cam1b = 1
-                        cam1c = 1
-                        cam2a = 1
-                        cam2b = 1
-                        cam3  = 1
-                        cam4a = 1
-                        cam4b = 1
-                        cam5  = 1
-                        cam6  = 1
-                        cam7  = 1
-                        print('all cams enabled')
+                case "d_cam_all":
+                    cam1a = 0
+                    cam1b = 0
+                    cam1c = 0
+                    cam2a = 0
+                    cam2b = 0
+                    cam3  = 0
+                    cam4a = 0
+                    cam4b = 0
+                    cam5  = 0
+                    cam6  = 0
+                    cam7  = 0
+                    print('all cams enabled')
 
 # ---------------------------------------------------------------------------
 
@@ -514,22 +404,26 @@ def process_input(message):
             # Left door
                 case "ldoor":
                     if ActionChance(1,4) == 4:
-                        control(Ldoor, cams, direction, 1,     55, 355, 3, 455)
+                        direction = 1
+                        control(Ldoor, cams,      55, 355,     3, 455)
 
             # Right door
                 case "rdoor":
                     if ActionChance(1,4) == 4:
-                        control(Rdoor, cams, direction, 2,     1210, 350, 1278, 455)
+                        direction = 2
+                        control(Rdoor, cams,      1210, 350,   1278, 455)
 
             # Left light
                 case "llight":
                     if ActionChance(1,4) == 4:
-                        control(Llight, cams, direction, 1,    55, 455, 3, 455)
+                        direction = 1
+                        control(Llight, cams,     55, 455,     3, 455)
 
             # Right light
                 case "rlight":
                     if ActionChance(1,4) == 4:
-                        control(Rlight, cams, direction, 2,    1210, 470, 1278, 455)
+                        direction = 2
+                        control(Rlight, cams,     1210, 470,   1278, 455)
 
 
             # general camera
@@ -555,7 +449,7 @@ def process_input(message):
                                 return
 
             # different cams
-                case "start":
+                case "cam1a":
                     if cam1a == 1:
                         camera(cams, 980,350)
 
@@ -563,7 +457,7 @@ def process_input(message):
                     if cam1b == 1:
                         camera(cams, 980,400)
 
-                case "foxy":
+                case "cam1c":
                     if cam1c == 1:
                         camera(cams, 920,480)
 
