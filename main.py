a3d408def1b5c4ae378c9376d920e193397a1f18
blanana <79705799+blanana@users.noreply.github.com>
2022-11-11 23:05:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ from keyboard import is_pressed
 from threading import Thread
 from pydirectinput import moveTo
 
-message, user, direction, stop, cams, ran = ' ', ' ', 2, 1, 0, 0
-ld, rd, ll, rl, cameras, boop = 0, 0, 0, 0, 0, 0
-cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+message, user = ' ', ' '
+
+#       direction, stop, camera, ran
+main = [        2,    1,      0,   0]
+
+#           Ldoor, Rdoor, Llight, Rlight, Cameras, boop
+controls = [    0,     0,      0,      0,       0,    0]
+
+#          cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7
+cameras = [    0,     0,     0,     0,     0,    0,     0,     0,    0,    0,    0]
 
 pydirectinput.FAILSAFE = False
 
@@ -100,29 +107,7 @@ def twitch():
                 process_input(message)
 
 # -----------------------------------------------------------------------------------------------------------------------
-# miscellaneous functions
-
-def camera(cam, x, y):
-    if cam == 1:
-        moveTo(x, y)
-        click('left')
-
-def ActionChance(x,y):
-    global ran
-    if ran == 1:
-        chance = randint(x,y)
-        return chance
-    else:
-        chance = 4
-        return chance
-
-def control(com1, com2, x1, y1, x2, y2):
-    if not com1 == 1 or not com2 == 0:
-        return
-    moveTo(x1, y1)
-    sleep(0.44)
-    click('left')
-    moveTo(x2, y2)
+# Functions
 
 def toggle(var, text, val, a, b):
     if var == a:
@@ -134,245 +119,195 @@ def toggle(var, text, val, a, b):
         sleep(val)
         return a
 
+
+def ActionChance(x,y):
+    if main[3] == 1:
+        return randint(x,y)
+    else:
+        return 4
+
+
+def control(com1, com2, x1, y1, x2, y2):
+    if com1 == 1 and com2 == 0:
+            moveTo(x1, y1)
+            sleep(0.44)
+            click('left')
+            moveTo(x2, y2)
+
+
+def camera(com1, com2, x1, y1):
+    if com1 == 1 and com2 == 1:
+            moveTo(x1, y1)
+            click('left')
+
+
+def cams(z, x, y, x1, y1):
+    moveTo(x, y)
+    moveTo(x1,y1)
+    moveTo(x, y)
+    return z
+
 # -----------------------------------------------------------------------------------------------------------------------
 # hotkeys
 
 def hotkey():
-    global direction, stop, cams, ran
-    global ld, rd, ll, rl, cameras, boop
-    global cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7
     while True:
 
-# ---------------------------------------------------------------------------
-# toggle hotkeys
+        if is_pressed('L'):
+            main[1] = toggle(main[1], 'Commands', 0.2, 0, 1)
 
-    # Left door toggle
         if is_pressed('1'):
-            ld = toggle(ld, 'Left door', 0.2, 1, 0)
+            controls[0] = toggle(controls[0], 'Left door', 0.2, 1, 0)
 
-    # Right door toggle
         if is_pressed('2'):
-            rd = toggle(rd, 'Right door', 0.2, 1, 0)
+            controls[1] = toggle(controls[1], 'Right door', 0.2, 1, 0)
 
-    # Left light toggle
         if is_pressed('3'):
-            ll = toggle(ll, 'Left light', 0.2, 1, 0)
+            controls[2] = toggle(controls[2], 'Left light', 0.2, 1, 0)
 
-    # Right light toggle
         if is_pressed('4'):
-            rl = toggle(rl, 'Right light', 0.2, 1, 0)
+            controls[3] = toggle(controls[3], 'Right light', 0.2, 1, 0)
 
-    # Camera toggle
         if is_pressed('5'):
-            cameras = toggle(cameras, 'Cameras', 0.2, 1, 0)
+            controls[4] = toggle(controls[4], 'Cameras', 0.2, 1, 0)
 
-    # Boop toggle
         if is_pressed('6'):
-            boop = toggle(boop, 'Boop', 0.2, 1, 0)
+            controls[5] = toggle(controls[5], 'boop', 0.2, 1, 0)
 
-    # Chance toggle
         if is_pressed('7'):
-            ran = toggle(ran, 'Chance', 0.2, 1, 0)
-
-    # command toggle
-        if is_pressed('l'):
-            stop = toggle(stop, 'Commands', 0.2, 0, 1)
-
-    # reset
-        if is_pressed('r'):
-            cams, direction = 0, 2
-            ld, rd, ll, rl, cameras, boop = 0, 0, 0, 0, 0, 0
-            cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7 = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-            print('reset')
-            sleep(0.5)
-
-    # test
-        if is_pressed('t'):
-            print("                    ")
-            print("direction:" + str(direction))
-            print("cams:"      + str(cams))
-            print("chance:"    + str(ran))
-            print("stop:"      + str(stop))
-            print("--------------------")
-            print("Ldoor:"   + str(ld))
-            print("Rdoor:"   + str(rd))
-            print("Llight:"  + str(ll))
-            print("Rlight:"  + str(rl))
-            print("cameras:" + str(cameras))
-            print("boop:"    + str(boop))
-            print("--------------------")
-            print("cam1a:" + str(cam1a))
-            print("cam1b:" + str(cam1b))
-            print("cam1c:" + str(cam1c))
-            print("cam2a:" + str(cam2a))
-            print("cam2b:" + str(cam2b))
-            print("cam3:"  + str(cam3))
-            print("cam4a:" + str(cam4a))
-            print("cam4b:" + str(cam4b))
-            print("cam5:"  + str(cam5))
-            print("cam6:"  + str(cam6))
-            print("cam7:"  + str(cam7))
-            print("                    ")
-            sleep(0.5)
+            main[3] = toggle(main[3], 'Chance', 0.2, 1, 0)
 
 # -----------------------------------------------------------------------------------------------------------------------
 # game control
 
 def process_input(message):
-
-    global user, direction, stop, cams, ran
-    global ld, rd, ll, rl, cameras, boop
-    global cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7
-
-# ---------------------------------------------------------------------------
-
     if user.lower() == 'blanana_m' or 'astralspiff':
         match str(message).lower():
 
-        # toggle individual cameras
-                case "t_cam1a":
-                    cam1a = toggle(cam1a, 'Camera 1a', 0, 1, 0)
+            case "t_cam1a":
+                cameras[0]  = toggle(cameras[0], 'Camera 1a', 0, 1, 0)
 
-                case "t_cam1b":
-                    cam1b = toggle(cam1b, 'Camera 1b', 0, 1, 0)
+            case "t_cam1b":
+                cameras[1]  = toggle(cameras[1], 'Camera 1b', 0, 1, 0)
 
-                case "t_cam1c":
-                    cam1c = toggle(cam1c, 'Camera 1c', 0, 1, 0)
+            case "t_cam1c":
+                cameras[2]  = toggle(cameras[2], 'Camera 1c', 0, 1, 0)
 
-                case "t_cam2a":
-                    cam2a = toggle(cam2a, 'Camera 2a', 0, 1, 0)
+            case "t_cam2a":
+                cameras[3]  = toggle(cameras[3], 'Camera 2a', 0, 1, 0)
 
-                case "t_cam2b":
-                    cam2b = toggle(cam2b, 'Camera 2b', 0, 1, 0)
+            case "t_cam2b":
+                cameras[4]  = toggle(cameras[4], 'Camera 2b', 0, 1, 0)
 
-                case "t_cam3":
-                    cam3  = toggle(cam3, 'Camera 3', 0, 1, 0)
+            case "t_cam3":
+                cameras[5]  = toggle(cameras[5], 'Camera 3', 0, 1, 0)
 
-                case "t_cam4a":
-                    cam4a = toggle(cam4a, 'Camera 4a', 0, 1, 0)
+            case "t_cam4a":
+                cameras[6]  = toggle(cameras[6], 'Camera 4a', 0, 1, 0)
 
-                case "t_cam4b":
-                    cam4b = toggle(cam4b, 'Camera 4b', 0, 1, 0)
+            case "t_cam4b":
+                cameras[7]  = toggle(cameras[7], 'Camera 4b', 0, 1, 0)
 
-                case "t_cam5":
-                    cam5  = toggle(cam5, 'Camera 5', 0, 1, 0)
+            case "t_cam5":
+                cameras[8]  = toggle(cameras[8], 'Camera 5', 0, 1, 0)
+            
+            case "t_cam6":
+                cameras[9]  = toggle(cameras[9], 'Camera 6', 0, 1, 0)
 
-                case "t_cam6":
-                    cam6  = toggle(cam6, 'Camera 6', 0, 1, 0)
+            case "t_cam7":
+                cameras[10] = toggle(cameras[10], 'Camera 7', 0, 1, 0)
 
-                case "t_cam7":
-                    cam7  = toggle(cam7, 'Camera 7', 0)
+            case "e_cam_all":
+                cameras = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                print('all cameras enabled')
 
-                case "e_cam_all":
-                    cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7 = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-                    print('all cams enabled')
+            case "1_cam_all":
+                cameras = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                print('all cameras disabled')
 
-                case "d_cam_all":
-                    cam1a, cam1b, cam1c, cam2a, cam2b, cam3, cam4a, cam4b, cam5, cam6, cam7 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                    print('all cams enabled')
-
-# ---------------------------------------------------------------------------
-
-    if stop == 0:
+    if main[1] == 0:
         match str(message).lower():
 
-            # Left door
                 case "ldoor":
                     if ActionChance(1,4) == 4:
-                        direction = 1
-                        control(ld, cams, 55,355, 3,455)
+                        main[0] = 1
+                        control(controls[0], main[2], 55, 355, 3, 455)
 
-            # Right door
                 case "rdoor":
                     if ActionChance(1,4) == 4:
-                        direction = 2
-                        control(rd, cams, 1210,350, 1278,455)
+                        main[0] = 2
+                        control(controls[1], main[2], 1210, 350, 1278, 455)
 
-            # Left light
                 case "llight":
                     if ActionChance(1,4) == 4:
-                        direction = 1
-                        control(ll, cams, 55,455, 3,455)
+                        main[0] = 1
+                        control(controls[2], main[2], 55, 455, 3, 455)
 
-            # Right light
                 case "rlight":
                     if ActionChance(1,4) == 4:
-                        direction = 2
-                        control(rl, cams, 1210,470, 1278,455)
+                        main[0] = 2
+                        control(controls[3], main[2], 1210, 470, 1278, 455)
 
-            # general camera
+
                 case "cams":
-                    if not ActionChance(1,4) == 4 or not cameras == 1:
-                        return
-                    if cams == 0:
-                        cams = 1
-                        moveTo(550,606)
-                        moveTo(562,700)
-                        moveTo(550,606)
-                        return
-                    if cams == 1:
-                        cams = 0
-                        moveTo(550,606)
-                        moveTo(562,667)
-                        moveTo(550,606)
-                        return
+                    if controls[4] == 1 and ActionChance(1,4) == 4:
+                            if main[2] == 0:
+                                main[2] = cams(1, 550, 606, 562, 700)
+                                return
+                            if main[2] == 1:
+                                main[2] = cams(0, 550, 606, 562, 700)
 
-            # different cams
+
                 case "cam1a":
-                    if cam1a == 1:
-                        camera(cams, 980,350)
+                    camera(cameras[0], main[3], 980, 350)
 
                 case "cam1b":
-                    if cam1b == 1:
-                        camera(cams, 980,400)
+                    camera(cameras[1], main[3], 980, 350)
 
                 case "cam1c":
-                    if cam1c == 1:
-                        camera(cams, 920,480)
+                    camera(cameras[2], main[3], 980, 350)
 
                 case "cam2a":
-                    if cam2a == 1:
-                        camera(cams, 980,600)
+                    camera(cameras[3], main[3], 980, 350)
 
                 case "cam2b":
-                    if cam2b == 1:
-                        camera(cams, 980,650)
+                    camera(cameras[4], main[3], 980, 350)
 
                 case "cam3":
-                    if cam3 == 1:
-                        camera(cams, 900,580)
+                    camera(cameras[5], main[3], 980, 350)
 
                 case "cam4a":
-                    if cam4a == 1:
-                        camera(cams, 1080,600)
+                    camera(cameras[6], main[3], 980, 350)
 
                 case "cam4b":
-                    if cam4b == 1:
-                        camera(cams, 1080,650)
+                    camera(cameras[7], main[3], 980, 350)
 
                 case "cam5":
-                    if cam5 == 1:
-                        camera(cams, 850,430)
+                    camera(cameras[8], main[3], 980, 350)
 
                 case "cam6":
-                    if cam6 == 1:
-                        camera(cams, 1200,570)
+                    camera(cameras[9], main[3], 980, 350)
 
                 case "cam7":
-                    if cam7 == 1:
-                        camera(cams, 1200,430)
+                    camera(cameras[10], main[3], 980, 350)
 
-            # boop Freddy's nose - "the most important control"
+
                 case "boop":
-                    if not ActionChance(1,4) == 4 or not boop == 1 or not direction == 1 or not cams == 0:
-                        return
-                    moveTo(679,236)
-                    click('left')
-                    print('boop!')
-                    moveTo(3,455)
+                    if controls[5] == 1 and main[2] == 0 and ActionChance(1,4) == 4:
+                        if main[0] == 1:
+                            moveTo(679,236)
+                            click('left')
+                            moveTo(3,455)
+                        if main[0] == 2:
+                            moveTo(3,455)
+                            sleep(0.44)
+                            moveTo(679,236)
+                            click('left')
+                            moveTo(3,455)
+                            main[0] = 1
 
 # -----------------------------------------------------------------------------------------------------------------------
 # threading
 
-Thread(target=twitch).start()
 Thread(target=hotkey).start()
+Thread(target=twitch).start()
